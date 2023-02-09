bc63e2bcf3db2e54a48aa1ef07ba1b8f3044347b
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-02-09 14:04:07 +0700

    tabPanel

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
 import CategoryHome from "@/components/home/dashboard/categoryHome";
+import TabService from "@/components/home/dashboard/tabService";
 
 const classes = {
   root: {
@@ -24,7 +25,9 @@ export default function Home() {
               {/*But will be 6 units on small screens */}
               <CategoryHome />
               <Grid item xs={12} sm={6}>
-                <Paper style={classes.paper}>xs=12 sm=6</Paper>
+                <Paper style={classes.paper}>
+                    <TabService />
+                </Paper>
               </Grid>
               <Grid item xs={6} sm={3}>
                 <Paper style={classes.paper}>xs=6 sm=3</Paper>

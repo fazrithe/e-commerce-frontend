ea009c0cccd189fbde32281193a5c02052ecf837
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-02-09 17:07:42 +0700

    update footer

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -4,11 +4,10 @@ import Typography from "@mui/joy/Typography"
 
 export default function Footer() {
     return(
-        <Grid item xs={12} sm={6}>
-        <CardContent sx={{ justifyContent: 'flex-end' }}>
-        
-          Yosemite National Park
-      </CardContent>
+    <Grid item xs={12} sm={6} className="mt-4 mb-2">
+            <CardContent sx={{ justifyContent: 'flex-end', textAlign: 'center' }}>
+                E-Commerce
+            </CardContent>
         </Grid>
     )
 }
